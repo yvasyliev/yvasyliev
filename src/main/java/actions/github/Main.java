2d4533102c907e76615f690a2f446e8c2827f022
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-09-08 21:12:14 +0300

    result2

--- src/main/java/actions/github/Main.java
+++ src/main/java/actions/github/Main.java
@@ -2,6 +2,8 @@ package actions.github;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("result=Hello World!");
+        System.out.println("""
+                result=Hello World!
+                result2=Second result!""");
     }
 }
